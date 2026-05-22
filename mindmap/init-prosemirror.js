ea57ccebd0b1b5be5ca32499053a400717a5f0bf
punketieren <pasha.ustal@gmail.com>
2026-05-22 17:18:39 +0300

    Update init-prosemirror.js

--- mindmap/init-prosemirror.js
+++ mindmap/init-prosemirror.js
@@ -260,36 +260,7 @@ setTimeout(async () => {
         ;  } })();
 		
 		//Подсветка кнопок
-		function updateButtonState() {
-    const { state } = window.editorView;
-    const { from, to } = state.selection;
-    
-    //подсветка-Марки (жирный, курсив, зачёркивание)
-    const marks = ['strong', 'em', 'strike'];
-    marks.forEach(markName => {
-        const mark = state.schema.marks[markName];
-        const isActive = mark && state.doc.rangeHasMark(from, to, mark);
-        const btn = document.getElementById(markName);
-        if (btn) btn.classList.toggle('active', isActive);
-    });
-    
-    // Узлы (списки, цитаты)
-    const { $from } = state.selection;
-    const parent = $from.node($from.depth);
-    
-    const ulBtn = document.getElementById('ul');
-    const olBtn = document.getElementById('ol');
-    const quoteBtn = document.getElementById('quote');
-    
-    if (ulBtn) ulBtn.classList.toggle('active', parent.type.name === 'bullet_list');
-    if (olBtn) olBtn.classList.toggle('active', parent.type.name === 'ordered_list');
-    if (quoteBtn) quoteBtn.classList.toggle('active', parent.type.name === 'blockquote');
-}
-
-// Подписка на события "статус кнопки"
-window.editorView.dom.addEventListener('click', updateButtonState);
-window.editorView.dom.addEventListener('keyup', updateButtonState);
-window.editorView.dom.addEventListener('selectionchange', updateButtonState);
+		
 		
 		
 //		const savePngBtn = document.getElementById('save-png');
